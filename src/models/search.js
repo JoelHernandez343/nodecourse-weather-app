f3e904738ca9f20374215fcb20d0671814a3fb36
JoelHernandez343 <joel.programador@gmail.com>
2021-04-10 14:19:13 -0500

    Added dotenv package and configured API token

--- src/models/search.js
+++ src/models/search.js
@@ -3,9 +3,22 @@ const axios = require('axios').default;
 class Searches {
   constructor() {}
 
+  get _ParamsMapbox() {
+    return {
+      access_token: process.env.MAPBOX_KEY,
+      limit: 5,
+      language: 'en',
+    };
+  }
+
   async city(place = '') {
     try {
-      const rest = await axios.get('https://reqres.in/api/users?page=2');
+      const instance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json`,
+        params: this._ParamsMapbox,
+      });
+
+      const rest = await instance.get();
       console.log(rest.data);
     } catch (err) {
       console.log(err);
